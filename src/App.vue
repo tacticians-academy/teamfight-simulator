e88cc35073c08af6461e93d39f7e3d73f2629afa
Kyle <kylecoburn@gmail.com>
2021-12-28 20:55:39 -0800

    ♻️ sidebar component

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,12 @@
 import '#/assets/main.postcss'
 
 import Board from '#/components/Board.vue'
-import SelectUnits from '#/components/SelectUnits.vue'
-
-import { SIDEBAR_UNITS } from '#/game/constants'
-
-function onPlay() {
-	console.log('todo')
-}
+import Sidebar from '#/components/Sidebar.vue'
 </script>
 
 <template>
 <div class="h-full  flex">
-	<div class="sidebar  overflow-y-scroll bg-gray-100  flex flex-col justify-between">
-		<div class="p-1 flex flex-col">
-			<SelectUnits />
-		</div>
-		<button :disabled="true" class="button" @click="onPlay">Fight!</button>
-	</div>
+	<Sidebar class="overflow-y-scroll" />
 	<Board class="overflow-y-scroll" />
 </div>
 </template>
-
-<style scoped lang="postcss">
-.sidebar {
-	width: v-bind(SIDEBAR_UNITS);
-}
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import '#/assets/main.postcss'
+
+import SelectUnits from '#/components/SelectUnits.vue'
+
+import { SIDEBAR_UNITS } from '#/game/constants'
+
+function onPlay() {
+	console.log('todo')
+}
+</script>
+
+<template>
+<div class="sidebar  bg-gray-100  flex flex-col justify-between">
+	<div class="p-1 flex flex-col">
+		<SelectUnits />
+	</div>
+	<button :disabled="true" class="button" @click="onPlay">Fight!</button>
+</div>
+</template>
+
+<style scoped lang="postcss">
+.sidebar {
+	width: v-bind(SIDEBAR_UNITS);
+}
+</style>
