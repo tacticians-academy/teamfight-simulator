105cff9583d0a44e818280b300a8daebf1757da5
Kyle <kylecoburn@gmail.com>
2022-01-16 20:36:25 -0800

    🏗 getDensestTargetHexes

--- src/helpers/boardUtils.ts
+++ src/helpers/boardUtils.ts
@@ -1,13 +1,13 @@
 import type { ChampionUnit } from '#/game/unit'
 
 import { BOARD_COL_COUNT, BOARD_ROW_COUNT } from '#/helpers/constants'
-import type { HexCoord } from '#/helpers/types'
+import type { HexCoord, TeamNumber } from '#/helpers/types'
 
 const lastCol = BOARD_COL_COUNT - 1
 const lastRow = BOARD_ROW_COUNT - 1
 
-export function buildBoard(fillObjects: boolean): any[][] {
-	return [...Array(BOARD_ROW_COUNT)].map(row => [...Array(BOARD_COL_COUNT)].map(col => (fillObjects ? {} : [])))
+export function buildBoard(fillObjects: boolean | 0): any[][] {
+	return [...Array(BOARD_ROW_COUNT)].map(row => [...Array(BOARD_COL_COUNT)].map(col => (fillObjects === 0 ? 0 : (fillObjects ? {} : []))))
 }
 
 function nearestAvailableRecursive(hex: HexCoord, unitPositions: HexCoord[]): HexCoord | null {
@@ -33,34 +33,64 @@ export function getClosestHexAvailableTo(startHex: HexCoord, units: ChampionUnit
 	return nearestAvailableRecursive(startHex, unitPositions)
 }
 
-export function getSurrounding([col, row]: HexCoord) {
-	const validHexes: HexCoord[] = []
-	if (col < lastCol) {
-		validHexes.push([col + 1, row])
-	}
-	if (col > 0) {
-		validHexes.push([col - 1, row])
+const surroundings = [
+	[[1, 0], [0, 1], [-1, 1]],
+	[[2, 0], [1, 1], [1, 2], [0, 2], [-1, 2], [-2, 1]],
+	[[3, 0], [2, 1], [2, 2], [1, 3], [0, 3], [-1, 3], [-2, 3], [-2, 2], [-3, 1]],
+]
+
+export function getSurrounding([col, row]: HexCoord, atDistance: number = 1): HexCoord[] {
+	if (atDistance < 1) {
+		return [[col, row]]
 	}
 	const isOffsetRow = row % 2 === 1
-	if (col > (isOffsetRow ? -1 : 0)) {
-		const lookLeft = isOffsetRow ? 0 : -1
-		if (row > 0) {
-			validHexes.push([col + lookLeft, row - 1])
-		}
-		if (row < lastRow) {
-			validHexes.push([col + lookLeft, row + 1])
-		}
-	}
-	if (col < lastCol + (isOffsetRow ? 0 : 1)) {
-		const lookRight = isOffsetRow ? 1 : 0
-		if (row > 0) {
-			validHexes.push([col + lookRight, row - 1])
-		}
-		if (row < lastRow) {
-			validHexes.push([col + lookRight, row + 1])
+	const validHexes: HexCoord[] = []
+	for (let mirror = 0; mirror < 2; mirror += 1) {
+		const mirrorMultiplier = mirror === 0 ? 1 : -1
+		for (const [colDelta, rowDelta] of surroundings[atDistance - 1]) {
+			const isOffsetFromRow = isOffsetRow && rowDelta % 2 !== 0
+			const newCol = col + (colDelta + (isOffsetFromRow ? 1 : 0)) * mirrorMultiplier
+			const newRow = row + rowDelta * mirrorMultiplier
+			if (newCol >= 0 && newCol <= lastCol && newRow >= 0 && newRow <= lastRow) {
+				validHexes.push([newCol, newRow])
+			}
 		}
 	}
 	return validHexes
+}
+
+export function getDensestTargetHexes(units: ChampionUnit[], team: TeamNumber | null, maxDistance: 1 | 2 | 3) {
+	const densityBoard = buildBoard(0)
+	let densestHexValue = 0
+	for (const unit of units) {
+		if (team !== null && unit.team !== team) {
+			continue
+		}
+		if (!unit.attackable()) {
+			continue
+		}
+		const position = unit.currentPosition()
+		for (let distance = 0; distance <= maxDistance; distance += 1) {
+			getSurrounding(position, distance).forEach(([col, row]) => {
+				const newValue = densityBoard[row][col] + maxDistance + 1 - distance
+				densityBoard[row][col] = newValue
+				if (newValue > densestHexValue) {
+					densestHexValue = newValue
+				}
+			})
+		}
+	}
+	const results: HexCoord[] = []
+	for (let rowIndex = 0; rowIndex < densityBoard.length; rowIndex += 1) {
+		const row = densityBoard[rowIndex]
+		for (let colIndex = 0; colIndex < row.length; colIndex += 1) {
+			const value = row[colIndex]
+			if (value === densestHexValue) {
+				results.push([colIndex, rowIndex])
+			}
+		}
+	}
+	return results
 }
 
 export function isSameHex(a: HexCoord, b: HexCoord) {
