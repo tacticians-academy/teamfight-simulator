29438a30f9d0f2557039f6a48842f6d6a937ce33
Kyle <kylecoburn@gmail.com>
2022-03-03 11:55:18 -0700

    ✨ Clockwork team effect

--- src/data/set6/traits.ts
+++ src/data/set6/traits.ts
@@ -1,6 +1,24 @@
+import { BonusKey } from '@tacticians-academy/academy-library'
+import type { TraitEffectData } from '@tacticians-academy/academy-library'
 import type { TraitKey } from '@tacticians-academy/academy-library/dist/set6/traits'
+
+import { state } from '#/game/store'
 
 import type { TraitEffectFn } from '#/helpers/types'
 
 export default {
+	Clockwork: {
+		team: (activeEffect: TraitEffectData) => {
+			const variables = []
+			const bonusPerAugment = activeEffect.variables['BonusPerAugment']
+			const bonusAS = activeEffect.variables['ASBonus']
+			if (bonusPerAugment != null) {
+				variables.push(['AS', state.augmentCount * bonusPerAugment * 100])
+			}
+			if (bonusAS != null) {
+				variables.push([BonusKey.AttackSpeed, bonusAS * 100])
+			}
+			return variables
+		},
+	},
 } as Record<TraitKey, { team?: TraitEffectFn }>
