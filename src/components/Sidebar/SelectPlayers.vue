83b806c30c0f1b553aee1dff3c8e1152e9381201
Kyle <kylecoburn@gmail.com>
2022-03-05 10:05:29 -0700

    🐛 disable setup with fieldset

--- src/components/Sidebar/SelectPlayers.vue
+++ src/components/Sidebar/SelectPlayers.vue
@@ -11,19 +11,21 @@ const { getters: { synergiesByTeam }, state } = useStore()
 
 <template>
 <div class="p-1">
-	<form :disabled="state.isRunning">
-		<div>
-			<label for="select-stage" class="mr-1">Stage:</label>
-			<select id="select-stage" v-model="state.stageNumber">
-				<option v-for="stageNumber in 9" :key="stageNumber">{{ stageNumber }}</option>
-			</select>
-		</div>
-		<div>
-			<label for="select-mutant" class="mr-1">Mutants:</label>
-			<select id="select-mutant" v-model="state.mutantType" :disabled="state.isRunning">
-				<option v-for="type in MutantType" :key="type">{{ type }}</option>
-			</select>
-		</div>
+	<form>
+		<fieldset :disabled="state.isRunning">
+			<div>
+				<label for="select-stage" class="mr-1">Stage:</label>
+				<select id="select-stage" v-model="state.stageNumber">
+					<option v-for="stageNumber in 9" :key="stageNumber">{{ stageNumber }}</option>
+				</select>
+			</div>
+			<div>
+				<label for="select-mutant" class="mr-1">Mutants:</label>
+				<select id="select-mutant" v-model="state.mutantType">
+					<option v-for="type in MutantType" :key="type">{{ type }}</option>
+				</select>
+			</div>
+		</fieldset>
 	</form>
 	<div v-for="(teamSynergies, teamIndex) in synergiesByTeam" :key="teamIndex">
 		<div class="font-semibold">Team {{ getTeamName(teamIndex) }}</div>
