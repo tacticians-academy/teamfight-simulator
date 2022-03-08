ad8152caa5c159a681aa5f17355aeaf24003af6a
Kyle <kylecoburn@gmail.com>
2022-03-08 09:00:47 -0700

    ✨🚧 Quicksilver item
    #129

--- src/data/items.ts
+++ src/data/items.ts
@@ -3,7 +3,7 @@ import type { ItemData } from '@tacticians-academy/academy-library'
 
 import { ItemKey } from '@tacticians-academy/academy-library/dist/set6/items'
 
-import type { BonusScaling, EffectResults } from '#/helpers/types'
+import type { BonusScaling, EffectResults, ShieldData } from '#/helpers/types'
 
 type ItemEffectFn = (item: ItemData) => EffectResults
 interface ItemFns {
@@ -29,6 +29,21 @@ export default {
 				console.log('Missing effects', item)
 			}
 			return { scalings }
+		},
+	},
+
+	[ItemKey.Quicksilver]: {
+		innate: (item) => {
+			const shields: ShieldData[] = []
+			const shieldDuration = item.effects['SpellShieldDuration']
+			if (shieldDuration != null) {
+				shields.push({
+					isSpellShield: true,
+					amount: 0, //TODO does not break
+					expiresAtMS: shieldDuration * 1000,
+				})
+			}
+			return { shields }
 		},
 	},
 
