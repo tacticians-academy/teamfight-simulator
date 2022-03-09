625c708cfd98611edf5580bbc913bf13c46605ee
Kyle <kylecoburn@gmail.com>
2022-03-08 17:25:33 -0700

    ⬆️ academy-library

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({ // https://vitejs.dev/config/
 	resolve: {
 		alias: {
 			'#/': path.join(path.resolve(), 'src', path.sep),
+			// '@tacticians-academy/academy-library/': path.join(path.resolve(), '..', 'academy-library', path.sep), //SAMPLE
+			// '@tacticians-academy/academy-library': path.join(path.resolve(), '..', 'academy-library/dist/index.ts'), //SAMPLE
 		},
 	},
 	server: {
