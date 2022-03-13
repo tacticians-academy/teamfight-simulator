e834763ab1aeecea29e4db736d0412b2bdaeeb73
Kyle <kylecoburn@gmail.com>
2022-03-13 12:45:33 -0700

    ➕ vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({ // https://vitejs.dev/config/
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'#/': path.join(path.resolve(), 'src', path.sep),
-			// '@tacticians-academy/academy-library/': path.join(path.resolve(), '..', 'academy-library', path.sep), //SAMPLE
-			// '@tacticians-academy/academy-library': path.join(path.resolve(), '..', 'academy-library/dist/index.ts'), //SAMPLE
-		},
-	},
+	plugins: [
+		tsconfigPaths({ loose: true }),
+		vue(),
+	],
 	server: {
 		open: true,
 	},
