125d53e42aa900957ca111ca3f6e2327d4f6cc02
Kyle <kylecoburn@gmail.com>
2022-03-21 21:26:40 -0700

    ✨ Stand United augment
    Closes #198

--- src/data/set6/augments.ts
+++ src/data/set6/augments.ts
@@ -3,12 +3,26 @@ import type { AugmentData } from '@tacticians-academy/academy-library'
 import { AugmentGroupKey } from '@tacticians-academy/academy-library/dist/set6/augments'
 
 import type { ChampionUnit } from '#/game/ChampionUnit'
+import type { BonusVariable, EffectResults, TeamNumber } from '#/helpers/types'
+import { getters } from '#/game/store'
 
 export interface AugmentFns {
-	enemyDeath?: (augment: AugmentData, elapsedMS: DOMHighResTimeStamp, dead: ChampionUnit, source: ChampionUnit) => number
+	apply?: (augment: AugmentData, team: TeamNumber, units: ChampionUnit[]) => void
+	enemyDeath?: (augment: AugmentData, elapsedMS: DOMHighResTimeStamp, dead: ChampionUnit, source: ChampionUnit) => void
 }
 
 export default {
+
+	[AugmentGroupKey.StandUnited]: {
+		apply: (augment, team, units) => {
+			const baseADAP = augment.effects['Resists']
+			if (baseADAP == null) {
+				return console.log('ERR', augment.name, augment.effects)
+			}
+			const bonusADAP = baseADAP * getters.synergiesByTeam.value[team].filter(({ activeEffect }) => !!activeEffect).length
+			units.forEach(unit => unit.addBonuses(AugmentGroupKey.StandUnited, [BonusKey.AbilityPower, bonusADAP], [BonusKey.AttackDamage, bonusADAP]))
+		},
+	},
 
 	[AugmentGroupKey.ThrillOfTheHunt]: {
 		enemyDeath: (augment, elapsedMS, dead, source) => {
