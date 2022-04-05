2c7bc4bd12595b26043cbb142d3f8558b08b22e6
Kyle <kylecoburn@gmail.com>
2022-04-05 15:16:47 -0700

    ✅ Jarvan IV

--- src/sim/data/set6/6.5/champions.test.ts
+++ src/sim/data/set6/6.5/champions.test.ts
@@ -9,3 +9,12 @@ import { fastForwardUntil } from '#/sim/loop'
 import { testSpawnUnits } from '#test/utils'
 
 beforeAll(async () => await setSetNumber(6.5))
+
+test(ChampionKey.JarvanIV, () => {
+	const [testUnit, [ally], [enemy]] = testSpawnUnits(ChampionKey.JarvanIV)
+	testUnit.castAbility(0, true)
+	fastForwardUntil(elapsedMS => testUnit.bonuses.length > 0)
+	expect(testUnit.attackSpeed()).toBeGreaterThan(testUnit.data.stats.attackSpeed)
+	expect(ally.attackSpeed()).toBeGreaterThan(ally.data.stats.attackSpeed)
+	expect(enemy.attackSpeed()).toBe(enemy.data.stats.attackSpeed)
+})
