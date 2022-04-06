bd48966c279465ca66032508668b0902f15cd6d5
Kyle <kylecoburn@gmail.com>
2022-04-05 17:34:00 -0700

    🔨🐛🚧 include vitest in production
    Workaround for not being able to specify tsconfig project path to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import vue from '@vitejs/plugin-vue'
 
+// export default defineConfig(({ mode }) => {
 export default defineConfig({ // https://vitejs.dev/config/
 	plugins: [
 		tsconfigPaths({ loose: true }),
