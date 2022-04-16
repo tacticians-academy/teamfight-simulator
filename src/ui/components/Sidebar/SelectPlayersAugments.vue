065161f943cb8f0e9e1a831a7b3d332d338835ab
Kyle <kylecoburn@gmail.com>
2022-04-15 17:13:36 -0700

    ✨♻️ search for augments

--- src/ui/components/Sidebar/SelectPlayersAugments.vue
+++ src/ui/components/Sidebar/SelectPlayersAugments.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import SelectPlayersAugmentsEntry from '#/ui/components/Sidebar/SelectPlayersAugmentsEntry.vue'
+
 import { computed, ref } from 'vue'
 
 import type { AugmentData, AugmentGroupKey } from '@tacticians-academy/academy-library'
@@ -36,6 +38,52 @@ const augmentGroups = computed(() => {
 })
 
 const maxAugmentCount = computed(() => Math.max(1, Math.min(3, state.stageNumber - 1)))
+
+// Search
+
+const searchText = ref('')
+
+function getSearchScore(searchWhole: string, searchWords: string[], testAgainst: string) {
+	testAgainst = testAgainst.toLowerCase()
+	const maxWordScore = searchWords.length + 1
+	let score = 0
+	if (testAgainst.includes(searchWhole)) {
+		score = testAgainst.startsWith(searchWhole) ? maxWordScore * 2 : maxWordScore
+	} else {
+		searchWords.forEach(word => {
+			if (testAgainst.includes(word)) {
+				score += testAgainst.startsWith(word) ? 2 : 1
+			}
+		})
+	}
+	return score
+}
+
+const searchAugments = computed(() => {
+	const searched = searchText.value.trim().toLowerCase().replaceAll(/\s+/g, ' ')
+	if (searched.length <= 1) return undefined
+	const searchWords = searched.split(' ')
+	const augmentScores = setData.activeAugments
+		.map(augment => {
+			let score = getSearchScore(searched, searchWords, augment.name)
+			if (score === 0) {
+				score = getSearchScore(searched, searchWords, augment.desc) / 2
+			}
+			return score > 0 ? [score, augment] : null
+		})
+		.filter((e): e is [number, AugmentData] => !!e)
+		.sort((a, b) => {
+			const scoreDiff = b[0] - a[0]
+			return scoreDiff !== 0 ? scoreDiff : (a[1].tier - b[1].tier)
+		})
+	return augmentScores.map(augmentScore => augmentScore[1])
+})
+
+function onSearchEnter() {
+	if (searchAugments.value?.length === 1) {
+		onAugment(searchAugments.value[0])
+	}
+}
 </script>
 
 <template>
@@ -59,7 +107,7 @@ const maxAugmentCount = computed(() => Math.max(1, Math.min(3, state.stageNumber
 	</div>
 </div>
 <div v-if="selectAugment" class=" absolute inset-0 z-50 overflow-y-scroll">
-	<div class="min-h-[100%] bg-gray-600/90   flex flex-col justify-center items-center">
+	<div class="min-h-[100%] bg-gray-600/90   flex flex-col items-center">
 		<div>
 			<button
 				v-for="(tier, tierIndex) in ['Silver', 'Gold', 'Prismatic']" :key="tierIndex"
@@ -70,29 +118,39 @@ const maxAugmentCount = computed(() => Math.max(1, Math.min(3, state.stageNumber
 			</button>
 			<button class="w-20 h-16 text-gray-400" @click="onAugment(null)">Clear</button>
 		</div>
+		<form @submit.prevent="onSearchEnter">
+			<input
+				v-model="searchText" v-focus
+				type="text" placeholder="Search for an augment..."
+				class="bg-primary mb-1 w-64 text-center rounded"
+			>
+		</form>
 		<div class="mb-8 space-y-4">
-			<div v-for="[label, augments] in augmentGroups" :key="label">
-				<div class="ml-1 text-gray-300">{{ label }}</div>
-				<div class="flex flex-wrap justify-center">
-					<button
-						v-for="augment in augments" :key="augment.name"
-						class="augment-box  group" :style="{ backgroundImage: augment ? `url(${getIconURLFor(state, augment)})` : undefined }"
-						@click="onAugment(augment)"
-					>
-						<span class="sidebar-icon-name  group-hover-visible">{{ augment.name }}</span>
-					</button>
-				</div>
+			<div v-if="searchAugments" class="flex flex-wrap justify-center">
+				<SelectPlayersAugmentsEntry
+					v-for="augment in searchAugments" :key="augment.name"
+					:augment="augment"
+					@click="onAugment(augment)"
+				/>
 			</div>
+			<template v-else>
+				<div v-for="[label, augments] in augmentGroups" :key="label">
+					<div class="ml-1 text-gray-300">{{ label }}</div>
+					<div class="flex flex-wrap justify-center">
+						<SelectPlayersAugmentsEntry
+							v-for="augment in augments" :key="augment.name"
+							:augment="augment"
+							@click="onAugment(augment)"
+						/>
+					</div>
+				</div>
+			</template>
 		</div>
 	</div>
 </div>
 </template>
 
 <style scoped lang="postcss">
-.augment-box {
-	@apply w-24 h-24 m-1 rounded-lg bg-cover text-white font-bold text-center  flex justify-center items-center;
-}
-
 .sidebar-icon {
 	@apply mb-1;
 	width: 9vw !important;
---
+++ src/ui/components/Sidebar/SelectPlayersAugmentsEntry.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+import type { AugmentData, AugmentGroupKey } from '@tacticians-academy/academy-library'
+
+import { useStore, setAugmentFor, setData } from '#/store/store'
+
+import { getIconURLFor } from '#/ui/helpers/utils'
+
+const { state } = useStore()
+
+const props = defineProps<{
+	augment: AugmentData
+}>()
+</script>
+
+<template>
+<button
+	class="augment-box  group" :style="{ backgroundImage: augment ? `url(${getIconURLFor(state, augment)})` : undefined }"
+>
+	<span class="sidebar-icon-name  group-hover-visible">{{ augment.name }}</span>
+</button>
+</template>
+
+<style scoped lang="postcss">
+.augment-box {
+	@apply w-24 h-24 m-1 rounded-lg bg-cover text-white font-bold text-center  flex justify-center items-center;
+}
+
+.sidebar-icon {
+	@apply mb-1;
+	width: 9vw !important;
+	height: 9vw !important;
+}
+</style>
