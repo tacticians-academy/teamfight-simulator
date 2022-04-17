16ca67b63b53abcae9e142fd9052d17712604a13
Kyle <kylecoburn@gmail.com>
2022-04-17 10:30:46 -0700

    🐛✅ Jarvin IV

--- src/sim/data/set6/6.5/champions.test.ts
+++ src/sim/data/set6/6.5/champions.test.ts
@@ -5,6 +5,7 @@ import { ChampionKey } from '@tacticians-academy/academy-library'
 import { setSetNumber } from '#/store/store'
 
 import { fastForwardUntil } from '#/sim/loop'
+import type { BonusLabelKey } from '#/sim/helpers/types'
 
 import { testSpawnUnits } from '#test/utils'
 
@@ -13,7 +14,7 @@ beforeAll(async () => await setSetNumber(6.5))
 test(ChampionKey.JarvanIV, () => {
 	const [testUnit, [ally], [enemy]] = testSpawnUnits(ChampionKey.JarvanIV)
 	testUnit.castAbility(0, true)
-	fastForwardUntil(elapsedMS => testUnit.bonuses.length > 0)
+	fastForwardUntil(elapsedMS => testUnit.getBonusesFrom(testUnit.getCurrentSpell()?.name as BonusLabelKey).length > 0)
 	expect(testUnit.attackSpeed()).toBeGreaterThan(testUnit.data.stats.attackSpeed)
 	expect(ally.attackSpeed()).toBeGreaterThan(ally.data.stats.attackSpeed)
 	expect(enemy.attackSpeed()).toBe(enemy.data.stats.attackSpeed)
