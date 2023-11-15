f676d6d0fe0f189a197cfc2f4edc1eba38cf28fa
Kyle <kylecoburn@gmail.com>
2023-11-15 09:38:46 -0800

    ⬆️

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 
-import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
 	const plugins = [tsconfigPaths({ loose: true }), vue()]
-	if (mode === 'development') {
-		plugins.push(checker({ vueTsc: true }))
-	}
 	return {
 		plugins,
 		server: {
